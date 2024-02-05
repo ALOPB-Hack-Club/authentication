f7a9234ddb9c3c546ae118268389a73497fa996d
HubertK05 <hubert.klimowicz.hk@gmail.com>
2024-02-05 19:19:35 +0000

    fix several template bugs

--- backend/main.ts
+++ backend/main.ts
@@ -1,3 +1,48 @@
-Deno.serve((_request: Request) => {
-  return new Response("Hello, world!");
+import { Application, Router } from "https://deno.land/x/oak@v12.6.2/mod.ts"
+import { Status } from "https://deno.land/x/oak_commons@0.4.0/status.ts";
+
+const app = new Application();
+const router = new Router();
+
+router.get("/", (ctx) => {
+  ctx.response.body = "Hi mom!"
 })
+
+router.get("/add/:a/:b", (ctx) => {
+  const { a, b } = ctx.params
+  const value = Number(a) + Number(b)
+  ctx.response.body = value
+})
+
+const dict: Record<string, string> = {}
+
+router.post("/dict/set/:key/:value", (ctx) => {
+  const { key, value } = ctx.params
+  dict[key] = value
+  ctx.response.status = Status.NoContent
+})
+
+router.get("/dict/get/:key", (ctx) => {
+  const { key } = ctx.params
+  ctx.response.body = dict[key]
+})
+
+const kv = await Deno.openKv()
+
+router.post("/register", async (ctx) => {
+  try {
+      const body = ctx.request.body({ type: "json" })
+      const credentials = await body.value
+      const key = ["users", credentials.login]
+      const value = { password: credentials.password }
+      await kv.set(key, value)
+      ctx.response.status = Status.NoContent
+  } catch {
+      ctx.response.status = Status.Unauthorized
+  }
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+await app.listen({ port: 8000 });
